042a1a35e1c5dc02183776dd48fb71b8353f124d
minhphan46 <21522345@gm.uit.edu.vn>
2023-10-24 17:28:06 +0700

    anchor

--- App.tsx
+++ App.tsx
@@ -5,10 +5,21 @@ import {
   ViroScene,
   Viro3DObject,
   Viro360Image,
+  ViroFlexView,
+  ViroImage,
   ViroTrackingState,
   ViroTrackingReason,
   ViroARSceneNavigator,
+  ViroBox,
+  ViroMaterials,
+  ViroAnimations,
+  ViroAmbientLight,
+  ViroARPlaneSelector,
+  ViroARPlane,
 } from '@viro-community/react-viro';
+import { Viro3DPoint } from '@viro-community/react-viro/dist/components/Types/ViroUtils';
+
+
 import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
 
@@ -26,9 +37,29 @@ function HelloWorldSceneAr(): JSX.Element {
     //   // Handle loss of tracking
     // }
   }
+  ViroMaterials.createMaterials({
+    wood: {
+      diffuseTexture: require('./assets/wood.jpg'),
+    },
+    anime: {
+      diffuseTexture: require('./assets/anime.jpg'),
+    },
+    waterColor: {
+      diffuseTexture: require('./assets/watercolor.jpg'),
+    }
+  });
+
+  ViroAnimations.registerAnimations({
+    rotate: {
+      duration: 2000,
+      properties: {
+        rotateY: '+=90',
+      },
+    },
+  });
 
   return (
-    <ViroARScene onTrackingUpdated={onInitialized}>
+    <ViroARScene>
       {/* <ViroText
         text={text}
         scale={[0.5, 0.5, 0.5]}
@@ -36,29 +67,91 @@ function HelloWorldSceneAr(): JSX.Element {
         style={styles.helloWorldTextStyle}
       /> */}
       {/* <Viro360Image source={require('./assets/landscape.jpeg')} /> */}
-      
-      <OBJTest></OBJTest>
+      <ViroARPlane minHeight={0.1} minWidth={0.1} alignment={'Horizontal'}>
+        <ViroBox position={[0, 0, 0]} scale={[0.1, 0.1, 0.1]} />
+        {/* <OBJTest></OBJTest> */}
+      </ViroARPlane>
     </ViroARScene>
+  );
+}
+
+interface CatBoxArguments {
+  position: Viro3DPoint
+}
+
+function CatBox (position : CatBoxArguments) : JSX.Element {
+  return (
+    <ViroNode position={position.position} scale={[1, 1, 1]}>
+        <ViroBox
+        height={2}
+        length={2}
+        width={2}
+        scale={[0.2, 0.2, 0.2]}
+        materials={['wood']}
+        animation={{name: 'rotate', loop: true, run: true}}></ViroBox>
+    </ViroNode>
+  );
+}
+function CatText (position : CatBoxArguments) : JSX.Element {
+  return (
+    <ViroNode position={position.position} scale={[0.5, 0.5, 0.5]}>
+        <ViroText style={styles.helloWorldTextStyle} text="Text A" position={[0, 0, 1]}  />
+      </ViroNode>
+  );
+}
+
+function Model3D (position : CatBoxArguments) : JSX.Element {
+  return (
+      <Viro3DObject
+        source={require("./assets/direction_arrow.glb")}
+        position={position.position}
+        scale={[0.5, 0.5, 0.5]}
+        rotation={[0, 90, 0]}
+        type="GLB"
+        materials={['wood']}
+      />
   );
 }
 const OBJTest = () => {
   return (
-    // <ViroText
-    //     text={"Hello askdjjhalskdhasldhsa"}
-    //     scale={[0.5, 0.5, 0.5]}
-    //     position={[0, 0, -1]}
-    //     style={styles.helloWorldTextStyle}
-    //   />
-    <>
-<ViroNode position={[0, 0, -1]} scale={[0.5, 0.5, 0.5]}>
-        <ViroText style={styles.helloWorldTextStyle} text="Text A" position={[0, 0, 1]}  />
-         
-      </ViroNode>
-      <ViroNode position={[0, 0, -2 ]} scale={[1, 1, 1]}>
-          <ViroText style={styles.helloWorldTextStyle} text="Text B" />
-        </ViroNode> 
-    </>
+  //   <> 
+  //   {
+  //     [1,2,3,4,5,6,7,8,9,10].map((item, index) => {
+  //       return <Model3D key ={index} position={[0,-5, -6 - index * 10]}></Model3D>
+  //     })
+  //   }
+  // </> 
+  
+  
+  //<ViroFlexView
+    //   style={{
+    //     flexDirection: 'row', 
+    //     padding: .1
+    //   }}
+    //   width={5.0} 
+    //   height={5.0}
+    //   position={[-5.0, 0.0, -2.0]}
+    //   rotation={[0, 70, 0]} >
+    //     <ViroImage
+    //       source={require('./assets/cat.jpg')} 
+    //       style={{flex: .5}} />
+    // </ViroFlexView>
     
+    // <Viro3DObject
+    //   source={require("./assets/ball.obj")}
+    //   position={[0, -5, -6]}
+    //   scale={[0.5, 0.5, 0.5]}
+    //   type="OBJ"
+    //   materials={['wood']}
+    // />
+
+    <ViroARPlaneSelector>
+    {
+      [1,2,3,4,5,6,7,8,9,10].map((object, index) => {
+        return <Model3D key={index} position={[0,0, 0 - index * 10]}></Model3D>
+      })
+    }
+    </ViroARPlaneSelector>
   )
 }
 function App(): JSX.Element {
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,46 @@
+// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+// /**
+//  * Metro configuration
+//  * https://facebook.github.io/metro/docs/configuration
+//  *
+//  * @type {import('metro-config').MetroConfig}
+//  */
+// const config = {};
+
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+'use strict';
+
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
+
+  const {
+    resolver: {assetExts},
+  } = defaultConfig;
+
+  // Thêm các phần mở rộng tài sản bạn muốn hỗ trợ
+  const newAssetExts = [
+    ...assetExts,
+    'obj',
+    'mtl',
+    'JPG',
+    'vrx',
+    'hdr',
+    'gltf',
+    'glb',
+    'GLB',
+    'bin',
+    'arobject',
+    'gif',
+  ];
+
+  return mergeConfig(defaultConfig, {
+    resolver: {
+      assetExts: newAssetExts,
+    },
+  });
+})();

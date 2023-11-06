cc6910873f32f82a44322691815ae5e4cbb1f17b
quangnhat22 <wuangsotech@gmail.com>
2023-11-06 10:48:53 +0700

    update bottom sheet ui

--- App.tsx
+++ App.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import ViroARSceneScreen from './src/pages/ViroARSceneScreen';
-import {SafeAreaView} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {Provider} from 'react-redux';
 import store from './src/store/store';
 import TestRedux from './src/pages/TestRedux';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
+    <GestureHandlerRootView style={styles.root}>
       <Provider store={store}>
         {/* <ViroARSceneScreen /> */}
         <TestRedux />
       </Provider>
-    </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
 
 export default App;

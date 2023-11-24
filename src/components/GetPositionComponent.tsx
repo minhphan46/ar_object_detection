c9a2774cba88bb6944bf3f3668746db913d33200
minhphan46 <21522345@gm.uit.edu.vn>
2023-11-24 16:47:33 +0700

    mapbox

--- src/components/GetPositionComponent.tsx
+++ src/components/GetPositionComponent.tsx
@@ -1,4 +1,3 @@
-import Geolocation from '@react-native-community/geolocation';
 import React, {useState} from 'react';
 import {Button, StyleSheet, Text, View} from 'react-native';
 
@@ -6,16 +5,6 @@ export default function GetPositionComponent() {
   const [position, setPosition] = useState<string | null>(null);
 
   const getCurrentPosition = () => {
-    Geolocation.getCurrentPosition(
-      pos => {
-        console.log(pos);
-        //setPosition(JSON.stringify(pos));
-        const {latitude, longitude} = pos.coords; // Trích xuất latitude và longitude
-        setPosition(`Lat: ${latitude}\nLong: ${longitude}`);
-      },
-      error => console.log(error),
-      {enableHighAccuracy: true},
-    );
   };
 
   return (
--- src/pages/PositionPage.tsx
+++ src/pages/PositionPage.tsx
@@ -5,107 +5,98 @@ import {
   Button,
   Image,
   StyleSheet,
+  Pressable,
 } from 'react-native';
 import React, {useState, useEffect} from 'react';
-import Geolocation from '@react-native-community/geolocation';
-import MapboxGL from '@rnmapbox/maps';
+import Mapbox, {
+  PointAnnotation,
+  UserLocation,
+  UserLocationRenderMode,
+  UserTrackingMode,
+} from '@rnmapbox/maps';
 
 const token =
   'pk.eyJ1IjoicXVhbmduaGF0MjIiLCJhIjoiY2xvaTJ3aTZ0MGN6czJycWhwMXZkdzh3aiJ9.rVhMy3XyQ9ilcYGjMFFtLw';
-MapboxGL.setWellKnownTileServer('Mapbox');
-MapboxGL.setAccessToken(token);
-MapboxGL.setConnected(true);
+Mapbox.setWellKnownTileServer('Mapbox');
+Mapbox.setAccessToken(token);
+Mapbox.setConnected(true);
 
 const PositionPage = () => {
-  const [position, setPosition] = useState<string | null>(null);
-  const [latitude, setLatitude] = useState<string | null>(null);
-  const [longitude, setLongitude] = useState<string | null>(null);
-  const [locationStatus, setLocationStatus] = useState('');
+  const [latitude, setLatitude] = useState<number>(0);
+  const [longitude, setLongitude] = useState<number>(0);
+
+  // list location.coords usestate
+  const [locationCoords, setLocationCoords] = useState<any>([]);
 
   useEffect(() => {
     return () => {
-      MapboxGL.setTelemetryEnabled(false);
+      Mapbox.setTelemetryEnabled(false);
     };
   }, []);
 
-  const getCurrentPositionGeoLocation = () => {
-    setLocationStatus('Getting Location ...');
-    Geolocation.getCurrentPosition(
-      pos => {
-        console.log(pos);
-        //setPosition(JSON.stringify(pos));
-        const {latitude, longitude} = pos.coords; // Trích xuất latitude và longitude
-        setPosition(`Lat: ${latitude}\nLong: ${longitude}`);
-        setLatitude(`${latitude}`);
-        setLongitude(`${longitude}`);
-        setLocationStatus('You are here');
-      },
-      error => {
-        setLocationStatus(error.message);
-        console.log(error);
-      },
-      {enableHighAccuracy: true},
-    );
-  };
-
-  const getCurrentPositionMapBox = () => {
-    setLocationStatus('Getting Location ...');
+  const handleUserLocationUpdate = (location: any) => {
+    console.log('location', location);
+    setLatitude(location.coords.latitude);
+    setLongitude(location.coords.longitude);
   };
 
   return (
     <View style={styles.page}>
-      <View style={styles.container1}>
-        <MapboxGL.MapView style={styles.map} />
+      <View style={styles.container}>
+        <Mapbox.MapView
+          style={styles.map}
+          onPress={e => {
+            console.log(e);
+          }}>
+          <Mapbox.UserLocation
+            minDisplacement={1}
+            onUpdate={handleUserLocationUpdate}
+            showsUserHeadingIndicator={true}
+            androidRenderMode="gps"
+            renderMode={UserLocationRenderMode.Normal}
+            requestsAlwaysUse={true}
+            visible={true}
+          />
+          <Mapbox.Camera
+            centerCoordinate={[longitude, latitude]}
+            zoomLevel={20}
+            animationMode={'flyTo'}
+            animationDuration={0}
+            followUserMode={UserTrackingMode.FollowWithHeading}
+            followHeading={0}
+          />
+          {locationCoords.map((item: any, index: number) => {
+            return (
+              <PointAnnotation
+                id="pointAnnotation"
+                coordinate={[item.longitude, item.latitude]}
+                onSelected={() => console.log('onSelected')}>
+                <Mapbox.Callout
+                  title={`long: ${item.longitude} \nlat: ${item.latitude}`}
+                />
+              </PointAnnotation>
+            );
+          })}
+        </Mapbox.MapView>
+        <Pressable
+          style={{
+            position: 'absolute',
+            bottom: 20,
+            right: 20,
+            backgroundColor: 'white',
+            borderRadius: 10,
+            padding: 20,
+          }}
+          onPress={() => {
+            setLocationCoords([
+              ...locationCoords,
+              {latitude: latitude, longitude: longitude},
+            ]);
+          }}>
+          <Text>Add</Text>
+        </Pressable>
       </View>
     </View>
-    // <SafeAreaView style={{flex: 1}}>
-    //   <View style={styles.container}>
-    //   <View style={styles.container}>
-    //     <Image
-    //       source={{
-    //         uri: 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/location.png',
-    //       }}
-    //       style={{width: 100, height: 100}}
-    //     />
-    //     <Text style={styles.boldText}>{locationStatus}</Text>
-    //     <Text
-    //       style={{
-    //         justifyContent: 'center',
-    //         alignItems: 'center',
-    //         marginTop: 16,
-    //       }}>
-    //       Longitude: {latitude}
-    //     </Text>
-    //     <Text
-    //       style={{
-    //         justifyContent: 'center',
-    //         alignItems: 'center',
-    //         marginTop: 16,
-    //       }}>
-    //       Latitude: {longitude}
-    //     </Text>
-    //     <View style={{marginTop: 20}}>
-    //       <Button title="Refresh" onPress={getCurrentPosition} />
-    //     </View>
-    //   </View>
-    //   <Text
-    //     style={{
-    //       fontSize: 18,
-    //       textAlign: 'center',
-    //       color: 'grey',
-    //     }}>
-    //     React Native Geolocation
-    //   </Text>
-    //   <Text
-    //     style={{
-    //       fontSize: 16,
-    //       textAlign: 'center',
-    //       color: 'grey',
-    //     }}>
-    //     www.aboutreact.com
-    //   </Text>
-    // </View>
-    // </SafeAreaView>
   );
 };
 
@@ -118,25 +109,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  container1: {
+  container: {
     height: '100%',
     width: '100%',
     backgroundColor: 'tomato',
   },
   map: {
     flex: 1,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  boldText: {
-    fontSize: 25,
-    color: 'red',
-    marginVertical: 16,
-    textAlign: 'center',
   },
 });

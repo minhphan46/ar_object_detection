b112d63e1b46ac60395e8935beffb4fea7c71a31
minhphan46 <21522345@gm.uit.edu.vn>
2023-11-30 14:52:10 +0700

    add line

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,7 +1,9 @@
 import {View, StyleSheet, Pressable, Image} from 'react-native';
 import React, {useState, useEffect} from 'react';
 import Mapbox, {
+  LineLayer,
   PointAnnotation,
+  ShapeSource,
   UserLocationRenderMode,
   UserTrackingMode,
 } from '@rnmapbox/maps';
@@ -20,6 +22,7 @@ const MapComponent = () => {
   );
 
   const [isShow, setIsShow] = useState(false);
+  const [isTouch, setIsTouch] = useState(false);
 
   const [latitude, setLatitude] = useState<any>(0);
   const [longitude, setLongitude] = useState<any>(0);
@@ -28,6 +31,11 @@ const MapComponent = () => {
     setLatitude(location.coords.latitude);
     setLongitude(location.coords.longitude);
   };
+
+  const lineCoordinates = [
+    [longitude, latitude],
+    [objectMapPosition.long, objectMapPosition.lat],
+  ];
 
   useEffect(() => {
     return () => {
@@ -38,9 +46,12 @@ const MapComponent = () => {
   return isShow ? (
     <View style={styles.container}>
       <Mapbox.MapView
-        compassEnabled={true}
-        compassFadeWhenNorth={true}
-        style={styles.map}>
+        // compassEnabled={true}
+        // compassFadeWhenNorth={true}
+        style={styles.map}
+        onTouchMove={() => {
+          setIsTouch(true);
+        }}>
         <Mapbox.UserLocation
           minDisplacement={1}
           onUpdate={handleUserLocationUpdate}
@@ -50,20 +61,45 @@ const MapComponent = () => {
           requestsAlwaysUse={true}
           visible={true}
         />
-        <Mapbox.Camera
-          centerCoordinate={[longitude, latitude]}
-          zoomLevel={18}
-          heading={direction.heading}
-          animationMode={'flyTo'}
-          animationDuration={0}
-          followUserMode={UserTrackingMode.FollowWithHeading}
-          followHeading={0}
-        />
+        {isTouch ? (
+          <Mapbox.Camera
+            centerCoordinate={[longitude, latitude]}
+            heading={direction.heading}
+            animationMode={'flyTo'}
+            animationDuration={0}
+            followUserMode={UserTrackingMode.FollowWithHeading}
+            followHeading={0}
+          />
+        ) : (
+          <Mapbox.Camera
+            centerCoordinate={[longitude, latitude]}
+            zoomLevel={18}
+            heading={direction.heading}
+            animationMode={'flyTo'}
+            animationDuration={0}
+            followUserMode={UserTrackingMode.FollowWithHeading}
+            followHeading={0}
+          />
+        )}
         <PointAnnotation
           id="pointAnnotation"
           coordinate={[objectMapPosition.long, objectMapPosition.lat]}>
           <View></View>
         </PointAnnotation>
+        <Mapbox.ShapeSource
+          id="lineSource"
+          shape={{type: 'LineString', coordinates: lineCoordinates}}>
+          <Mapbox.LineLayer
+            id="lineLayer"
+            style={{
+              lineColor: '#6DB9EF',
+              lineWidth: 2,
+              lineCap: 'round',
+              lineJoin: 'round',
+              lineDasharray: [0, 3],
+            }}
+          />
+        </Mapbox.ShapeSource>
       </Mapbox.MapView>
       <View style={styles.minusButton}>
         <Pressable
